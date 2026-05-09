642dad5204e4ddcf79bf5b9ff242f6c4939bd648
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2026-05-09 19:54:06 +0800

    Added docker container

--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,59 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-        react(),
-        svgr({
-            svgrOptions: {
-                plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
-                svgoConfig: {
-                    plugins: [
-                        {
-                            name: 'convertColors',
-                            params: {
-                                currentColor: true,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            react(),
+            svgr({
+                svgrOptions: {
+                    plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+                    svgoConfig: {
+                        plugins: [
+                            {
+                                name: 'convertColors',
+                                params: {
+                                    currentColor: true,
+                                },
                             },
-                        },
-                        {
-                            name: 'removeAttrs',
-                            params: {
-                                attrs: '(stroke-width)',
+                            {
+                                name: 'removeAttrs',
+                                params: {
+                                    attrs: '(stroke-width)',
+                                },
                             },
-                        },
-                    ],
+                        ],
+                    },
                 },
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@models_': path.resolve(__dirname, './src/components/three-models'),
+                '@utils': path.resolve(__dirname, './src/utils'),
+                '@context': path.resolve(__dirname, './src/context'),
+                '@managers': path.resolve(__dirname, './src/managers'),
+                '@pages': path.resolve(__dirname, './src/pages'),
+                '@styles': path.resolve(__dirname, './src/styles'),
+                '@components': path.resolve(__dirname, './src/components/ui-components'),
+                '@helpers': path.resolve(__dirname, './src/components/helpers'),
+                '@assets': path.resolve(__dirname, './src/assets'),
+                '@panels': path.resolve(__dirname, './src/components/panels'),
+                '@hooks': path.resolve(__dirname, './src/hooks'),
+                '@stores': path.resolve(__dirname, './src/stores'),
+                '@forms': path.resolve(__dirname, './src/components/forms'),
+                '@dialogs': path.resolve(__dirname, './src/components/dialogs'),
+                '@entity-items': path.resolve(__dirname, './src/components/entity-items'),
             },
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@models_': path.resolve(__dirname, './src/components/three-models'),
-            '@utils': path.resolve(__dirname, './src/utils'),
-            '@context': path.resolve(__dirname, './src/context'),
-            '@managers': path.resolve(__dirname, './src/managers'),
-            '@pages': path.resolve(__dirname, './src/pages'),
-            '@styles': path.resolve(__dirname, './src/styles'),
-            '@components': path.resolve(__dirname, './src/components/ui-components'),
-            '@helpers': path.resolve(__dirname, './src/components/helpers'),
-            '@assets': path.resolve(__dirname, './src/assets'),
-            '@panels': path.resolve(__dirname, './src/components/panels'),
-            '@hooks': path.resolve(__dirname, './src/hooks'),
-            '@stores': path.resolve(__dirname, './src/stores'),
-            '@forms': path.resolve(__dirname, './src/components/forms'),
-            '@dialogs': path.resolve(__dirname, './src/components/dialogs'),
-            '@entity-items': path.resolve(__dirname, './src/components/entity-items'),
         },
-    },
-    server: {
-        port: 5050,
-    },
+        server: {
+            port: parseInt(env.VITE_APP_PORT) || 5173,
+            host: true,
+        },
+    };
 });
